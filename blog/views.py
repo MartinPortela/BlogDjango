0f5a34941126829b348f91639f9855f6f5faf72a
Martín Portela <martinportelasegui@gmail.com>
2026-05-21 19:23:14 +0200

    Errores controlados y hecho para que el admin pueda editar y eliminar cualquier post

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,38 @@
 #blog/views.py
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
+from django.shortcuts import redirect
+from django.contrib import messages
 class VistaListaPosts(ListView):
     model = Post
     template_name = "home.html"
 class VistaDetallePost(DetailView):
     model = Post
     template_name = "detalle_post.html"
-class VistaCrearPost(CreateView): # uevo
+class VistaCrearPost(LoginRequiredMixin, CreateView): 
     model = Post
     template_name = "nuevo_post.html"
-    fields = ["titulo", "autor", "cuerpo"] 
-# Campos del modelo Post que se mostrarán en el formulario
-class VistaActualizarPost(UpdateView): # nuevo
+    fields = ["titulo", "cuerpo"] 
+    def form_valid(self, form):
+        form.instance.autor = self.request.user
+        return super().form_valid(form)
+class VistaActualizarPost(LoginRequiredMixin, UserPassesTestMixin, UpdateView): 
     model = Post
     template_name = "actualizar_post.html"
     fields = ["titulo", "cuerpo"]
-class VistaEliminarPost(DeleteView): # nuevo
+    def test_func(self):
+        return self.get_object().autor == self.request.user or self.request.user.id == 1
+    def handle_no_permission(self):
+        messages.error(self.request, "No tienes permiso para actualizar este post.")
+        return redirect("detalle_post", pk=self.get_object().pk)
+class VistaEliminarPost(LoginRequiredMixin, UserPassesTestMixin, DeleteView): 
     model = Post
     template_name = "eliminar_post.html"
     success_url = reverse_lazy("home")
+    def test_func(self):
+        return self.get_object().autor == self.request.user or self.request.user.id == 1
+    def handle_no_permission(self):
+        messages.error(self.request, "No tienes permiso para eliminar este post.")
+        return redirect("detalle_post", pk=self.get_object().pk)
